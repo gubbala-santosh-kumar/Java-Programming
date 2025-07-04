7420c64ccb74c0bc13f6c8a1a09d038d7321e981
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-04 15:40:59 +0530

    Create Java Code 3.java

---
+++ Java Code 3.java
@@ -0,0 +1,23 @@
+// Data Types
+public class Main {  
+    public static void main(String[] args)  
+    {  
+        int Num1 = 10;
+        System.out.println(Num1);
+        
+        // float Num2 = 2;  Syntax to declare a float value
+        float Num2 = 2.0f;  
+        System.out.println(Num2);  
+
+        double Num3 = 75.658;  
+        System.out.println(Num3);
+
+        char letter = 'A';
+        System.out.println(letter);
+
+        String Name= "Santosh";
+        System.out.println(Name);
+
+        // long, short, byte these are also data types
+    }  
+} 
