15981f70856f5b85b393c01ae30b80570943e9a7
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-04 15:25:23 +0530

    Create Java Code 2.java

---
+++ Java Code 2.java
@@ -0,0 +1,8 @@
+//Variables
+public class Main {
+    public static void main(String[] args) {
+        String name="Santosh Kumar";
+        int age = 21;
+        System.out.println("I'm "+name+" my age is "+age);
+    }
+}
