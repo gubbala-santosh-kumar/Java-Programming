7fc7168f4527a76db5d069a2c8305adf49120ca3
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-04 15:53:14 +0530

    Create Java Code 4.java

---
+++ Java Code 4.java
@@ -0,0 +1,40 @@
+// Operators
+/*
+Types of Operators:
+    1. Unary Operator
+    2. Arithmetic Operator
+    3. Relational Operator
+    4. Ternary Operator
+    5. Assignment Operator
+    6. Bitwise Operator
+    7. Logical Operator
+    8. Shift Operator
+*/
+public class Main {  
+    public static void main(String[] args)  
+    {  
+        //Airthmetic Operators
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        //Relational Operators
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+
+        //Logical Operators
+        System.out.println(a==a && b==b);
+        System.out.println(a==a || b==b);
+        System.out.println(!(a>b));
+
+        //Like this we have some other types of operators are there in java
+    }  
+} 
