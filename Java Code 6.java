b7a162bc9efeaaec4e39248f9cccdf8d5d433a8c
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-07 08:55:59 +0530

    Create Java Code 6.java

---
+++ Java Code 6.java
@@ -0,0 +1,12 @@
+// Strings
+// String Concatenation
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String firstName = sc.nextLine();
+	    String lastName = sc.nextLine();
+	    System.out.println("Hello "+firstName+" "+lastName);
+	}
+}
