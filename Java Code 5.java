727a236d2a5ca3d3a637cd3fab1c253cb68971f4
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-07 08:52:09 +0530

    Create Java Code 5.java

---
+++ Java Code 5.java
@@ -0,0 +1,11 @@
+//Type Casting
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int Num=sc.nextInt();
+	    float FloatNum  = Num;
+		System.out.println("Integer Value "+Num+"\nFloatValue "+FloatNum);
+	}
+}
