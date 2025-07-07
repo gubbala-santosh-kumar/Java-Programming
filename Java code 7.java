56d5ec9a7140c66b76d45a478ca1716e3a8045b6
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-07 11:28:17 +0530

    Create Java code 7.java

---
+++ Java code 7.java
@@ -0,0 +1,12 @@
+// Java Math
+public class Main
+{
+	public static void main(String[] args) {
+	    int num1 = 49;
+	    int num2 = 1;
+	    System.out.println(Math.max(num1,num2)); // Math.max()
+	    System.out.println(Math.min(num1,num2)); // Math.min()
+		System.out.println((int)Math.sqrt(num1)); // Math.sqrt()
+		System.out.println((int)(Math.random()*10)); // Math.random()
+	}
+}
