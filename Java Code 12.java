83fdbf81a19f2042aea08a12d4df65dd2db432ee
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-09 08:42:56 +0530

    Create Java Code 12.java

---
+++ Java Code 12.java
@@ -0,0 +1,13 @@
+// do while
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 1;
+		do{
+		    System.out.println(num);
+		    num++;
+		}while(num<=10);
+	}
+}
