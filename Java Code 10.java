2279e10bae75aae964359b55e9478f5d1ae198ca
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-09 08:36:45 +0530

    Create Java Code 10.java

---
+++ Java Code 10.java
@@ -0,0 +1,20 @@
+/*
+Loops
+Types of Loops:
+   while
+   do while
+   for
+*/
+// while loop
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 1;
+		while(num<=10){
+		    System.out.println(num);
+		    num++;
+		}
+	}
+}
