97e4e7e037a55908d0afda8d203ef276e955c5d5
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-09 08:39:42 +0530

    Create Java Code 11.java

---
+++ Java Code 11.java
@@ -0,0 +1,12 @@
+// for loop
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 1;
+		for(int i=num;i<=10;i++){
+		    System.out.println(i);
+		}
+	}
+}
